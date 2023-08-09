46e205c5c8efc4c269daa99ea894d7bb443cbf3b
FraNLxrD <francisco.medrano.jy@gmail.com>
2023-08-09 01:11:46 -0300

    deploy v5

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const path =require('path')
 const port =process.env.PORT || 3001;
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/Home.html'))
+    res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/login.html'))
+    res.sendFile(path.join(__dirname,'/views/login.html'))
 })
 app.get('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/register.html'))
+    res.sendFile(path.join(__dirname,'/views/register.html'))
 })
 app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'))
