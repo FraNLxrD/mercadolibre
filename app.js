d7ccf20e751966fface3a54cef5c6f195f3fe477
FraNLxrD <francisco.medrano.jy@gmail.com>
2023-08-09 01:37:20 -0300

    deploy v8.2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express= require('express')
 const app = express();
 const path =require('path')
 const publicPtah=path.resolve(__dirname,'./public')
-const port =process.env.PORT || 3001;
 app.use(express.static(publicPtah))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
@@ -13,4 +12,5 @@ app.get('/login',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/register.html'))
 })
+const port =process.env.PORT || 3001;
 app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'))
