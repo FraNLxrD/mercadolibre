a57eb94fb01666b7d173ea90889246227212a948
FraNLxrD <francisco.medrano.jy@gmail.com>
2023-08-09 01:35:04 -0300

    deploy v8.1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path =require('path')
 const publicPtah=path.resolve(__dirname,'./public')
 const port =process.env.PORT || 3001;
-app.use
+app.use(express.static(publicPtah))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
