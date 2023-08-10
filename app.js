6356d441c4f6219be6e07f8673631527da2443d9
FraNLxrD <francisco.medrano.jy@gmail.com>
2023-08-10 11:37:50 -0300

    deploy v8.8

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app = express();
 const path =require('path')
-const publicPtah=path.resolve(__dirname,'/public')
+const publicPtah=path.resolve(__dirname,'./public')
 app.use(express.static(publicPtah))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
